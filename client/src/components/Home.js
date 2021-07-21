5205ea2cfb59f41b239e149e7aca185ca287d86b
Eric Tedja <3ric8t@gmail.com>
2021-07-21 22:04:36 +0700

    added home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -27,14 +27,14 @@ const Home = () => {
               <div class="p-5">
                 <img
                   class="img-fluid rounded-circle"
-                  src="assets/img/01.jpg"
+                  src="assets/img/01.png"
                   alt="..."
                 />
               </div>
             </div>
             <div class="col-lg-6 order-lg-1">
               <div class="p-5">
-                <h2 class="display-4">For those about to rock...</h2>
+                <h2 class="display-4">Eric</h2>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
                   aliquid, mollitia odio veniam sit iste esse assumenda amet
@@ -53,14 +53,14 @@ const Home = () => {
               <div class="p-5">
                 <img
                   class="img-fluid rounded-circle"
-                  src="assets/img/02.jpg"
+                  src="assets/img/02.png"
                   alt="..."
                 />
               </div>
             </div>
             <div class="col-lg-6">
               <div class="p-5">
-                <h2 class="display-4">We salute you!</h2>
+                <h2 class="display-4">Arief</h2>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
                   aliquid, mollitia odio veniam sit iste esse assumenda amet
@@ -79,14 +79,67 @@ const Home = () => {
               <div class="p-5">
                 <img
                   class="img-fluid rounded-circle"
-                  src="assets/img/03.jpg"
+                  src="assets/img/03.png"
                   alt="..."
                 />
               </div>
             </div>
             <div class="col-lg-6 order-lg-1">
               <div class="p-5">
-                <h2 class="display-4">Let there be rock!</h2>
+                <h2 class="display-4">Asep</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                  aliquid, mollitia odio veniam sit iste esse assumenda amet
+                  aperiam exercitationem, ea animi blanditiis recusandae!
+                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section data-aos="fade-right" data-aos-anchor-placement="center-bottom">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6">
+              <div class="p-5">
+                <img
+                  class="img-fluid rounded-circle"
+                  src="assets/img/04.jpg"
+                  alt="..."
+                />
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="p-5">
+                <h2 class="display-4">Ilham</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                  aliquid, mollitia odio veniam sit iste esse assumenda amet
+                  aperiam exercitationem, ea animi blanditiis recusandae!
+                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section data-aos="fade-right" data-aos-anchor-placement="center-bottom">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6 order-lg-2">
+              <div class="p-5">
+                <img
+                  class="img-fluid rounded-circle"
+                  src="assets/img/05.jpg"
+                  alt="..."
+                />
+              </div>
+            </div>
+            <div class="col-lg-6 order-lg-1">
+              <div class="p-5">
+                <h2 class="display-4">Casey</h2>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
                   aliquid, mollitia odio veniam sit iste esse assumenda amet
