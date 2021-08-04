bf1b238e62860e15819b5e5e1cd2ec0e3431a818
casey <casey.khaled@gmail.com>
2021-08-04 20:34:52 +0700

    add more styling for login and register page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -56,63 +56,127 @@ const Login = () => {
     <div>
       <Navbar />
       <div className="login">
-        <div className="row mr-0 ml-0">
-          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-            <div data-aos="fade-right" className="card card-signin my-5">
-              <div className="card-body">
-                <h5 className="card-title text-center">Sign In</h5>
-                <form className="form-signin" onSubmit={onSubmitForm}>
-                  <div className="form-label-group">
-                    <input
-                      type="email"
-                      name="email"
-                      value={email}
-                      onChange={(e) => onChange(e)}
-                      id="inputEmail"
-                      className="form-control"
-                      placeholder="Email address"
-                      required
-                      autofocus
-                    />
-                    <label for="inputEmail">Email address</label>
+        <div className="row justify-content-center mx-0">
+          <div className="col-xl-10 col-lg-12 col-md-9">
+            <div data-aos="fade-right" className="card o-hidden border-0 shadow-lg my-5">
+              <div className="card-body p-0">
+                <div className="row">
+                  <div className="col-lg-6 d-none d-lg-block bg">
+                    <div className="p-5 mt-5">
+                      <div className="text-center text-white">
+                        <h1 className="card-title">AnyWallet</h1>
+                        <h2 className="card-title">Expense Tracker</h2>
+                        <i class="fas fa-wallet fa-10x"></i>
+                      </div>
+                    </div>
                   </div>
+                  <div className="col-lg-6">
+                    <div className="p-5">
+                      <div className="text-center">
+                        <h5 className="card-title mb-4">Welcome Back!</h5>
+                      </div>
+                      <form className="form-signin" onSubmit={onSubmitForm}>
+                        <div className="form-label-group">
+                          <input type="email" name="email" value={email} onChange={(e) => onChange(e)}
+                            id="inputEmail"
+                            className="form-control"
+                            placeholder="Email address"
+                            required
+                            autofocus
+                          />
+                          <label for="inputEmail">Email address</label>
+                        </div>
 
-                  <div className="form-label-group">
-                    <input
-                      type="password"
-                      name="password"
-                      value={password}
-                      onChange={(e) => onChange(e)}
-                      id="inputPassword"
-                      className="form-control"
-                      placeholder="Password"
-                      required
-                    />
-                    <label for="inputPassword">Password</label>
+                        <div className="form-label-group">
+                          <input type="password" name="password" value={password} onChange={(e) => onChange(e)}
+                            id="inputPassword"
+                            className="form-control"
+                            placeholder="Password"
+                            required
+                          />
+                          <label for="inputPassword">Password</label>
+                        </div>
+                        <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">
+                          Sign in
+                        </button>
+                        <hr className="my-4" />
+                        <Link to="/register" style={{ textDecoration: "none" }}>
+                          <button className="btn btn-lg btn-google btn-block text-uppercase" type="submit">
+                            <i className="fab mr-2"></i>
+                            Sign Up
+                          </button>
+                        </Link>
+                      </form>
+                    </div>
                   </div>
-                  <button
-                    className="btn btn-lg btn-primary btn-block text-uppercase"
-                    type="submit"
-                  >
-                    Sign in
-                  </button>
-                  <hr className="my-4" />
-                  <Link to="/register" style={{ textDecoration: "none" }}>
-                    <button
-                      className="btn btn-lg btn-google btn-block text-uppercase"
-                      type="submit"
-                    >
-                      <i className="fab mr-2"></i>
-                      Sign Up
-                    </button>
-                  </Link>
-                </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+
+    // <div>
+    //   <Navbar />
+    //   <div className="login">
+    //     <div className="row mr-0 ml-0">
+    //       <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+    //         <div data-aos="fade-right" className="card card-signin my-5">
+    //           <div className="card-body">
+    //             <h5 className="card-title text-center">Sign In</h5>
+    //             <form className="form-signin" onSubmit={onSubmitForm}>
+    //               <div className="form-label-group">
+    //                 <input
+    //                   type="email"
+    //                   name="email"
+    //                   value={email}
+    //                   onChange={(e) => onChange(e)}
+    //                   id="inputEmail"
+    //                   className="form-control"
+    //                   placeholder="Email address"
+    //                   required
+    //                   autofocus
+    //                 />
+    //                 <label for="inputEmail">Email address</label>
+    //               </div>
+
+    //               <div className="form-label-group">
+    //                 <input
+    //                   type="password"
+    //                   name="password"
+    //                   value={password}
+    //                   onChange={(e) => onChange(e)}
+    //                   id="inputPassword"
+    //                   className="form-control"
+    //                   placeholder="Password"
+    //                   required
+    //                 />
+    //                 <label for="inputPassword">Password</label>
+    //               </div>
+    //               <button
+    //                 className="btn btn-lg btn-primary btn-block text-uppercase"
+    //                 type="submit"
+    //               >
+    //                 Sign in
+    //               </button>
+    //               <hr className="my-4" />
+    //               <Link to="/register" style={{ textDecoration: "none" }}>
+    //                 <button
+    //                   className="btn btn-lg btn-google btn-block text-uppercase"
+    //                   type="submit"
+    //                 >
+    //                   <i className="fab mr-2"></i>
+    //                   Sign Up
+    //                 </button>
+    //               </Link>
+    //             </form>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -58,90 +58,195 @@ const Register = () => {
     <div>
       <Navbar />
       <div className="register">
-        <div className="row mr-0 ml-0">
-          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-            <div data-aos="fade-right" className="card card-signin my-5">
-              <div className="card-body">
-                <h5 className="card-title text-center">Sign Up</h5>
-                <form className="form-signin" onSubmit={onSubmitForm}>
-                  <div className="form-label-group">
-                    <input
-                      type="email"
-                      name="email"
-                      value={email}
-                      onChange={(e) => onChange(e)}
-                      id="inputEmail"
-                      className="form-control"
-                      placeholder="Email address"
-                      required
-                      autofocus
-                    />
-                    <label for="inputEmail">Email address</label>
+        <div className="row justify-content-center mx-0">
+          <div className="col-xl-10 col-lg-12 col-md-9">
+            <div data-aos="fade-right" className="card o-hidden border-0 shadow-lg my-5">
+              <div className="card-body p-0">
+                <div className="row">
+                  <div className="col-lg-6 d-none d-lg-block bg">
+                    <div className="p-5 mt-5">
+                      <div className="text-center text-white">
+                        <h1 className="card-title">AnyWallet</h1>
+                        <h2 className="card-title">Expense Tracker</h2>
+                        <i class="fas fa-wallet fa-10x"></i>
+                      </div>
+                    </div>
                   </div>
+                  <div className="col-lg-6">
+                    <div className="p-5">
+                      <div className="text-center">
+                        <h5 className="card-title">Sign Up!</h5>
+                      </div>
+                      <form className="form-signin" onSubmit={onSubmitForm}>
+                        <div className="form-label-group">
+                          <input
+                            type="email"
+                            name="email"
+                            value={email}
+                            onChange={(e) => onChange(e)}
+                            id="inputEmail"
+                            className="form-control"
+                            placeholder="Email address"
+                            required
+                            autofocus
+                          />
+                          <label for="inputEmail">Email address</label>
+                        </div>
 
-                  <div className="form-label-group">
-                    <input
-                      type="password"
-                      name="password"
-                      value={password}
-                      onChange={(e) => onChange(e)}
-                      id="inputPassword"
-                      className="form-control"
-                      placeholder="Password"
-                      required
-                    />
-                    <label for="inputPassword">Password</label>
-                  </div>
+                        <div className="form-label-group">
+                          <input
+                            type="password"
+                            name="password"
+                            value={password}
+                            onChange={(e) => onChange(e)}
+                            id="inputPassword"
+                            className="form-control"
+                            placeholder="Password"
+                            required
+                          />
+                          <label for="inputPassword">Password</label>
+                        </div>
 
-                  <div className="form-label-group">
-                    <input
-                      type="password"
-                      name="confirmPassword"
-                      value={confirmPassword}
-                      onChange={(e) => onChange(e)}
-                      id="inputConfPassword"
-                      className="form-control"
-                      placeholder="Confirm Password"
-                      required
-                    />
-                    <label for="inputConfPassword">Confirm Password</label>
-                  </div>
+                        <div className="form-label-group">
+                          <input
+                            type="password"
+                            name="confirmPassword"
+                            value={confirmPassword}
+                            onChange={(e) => onChange(e)}
+                            id="inputConfPassword"
+                            className="form-control"
+                            placeholder="Confirm Password"
+                            required
+                          />
+                          <label for="inputConfPassword">Confirm Password</label>
+                        </div>
 
-                  <div className="form-label-group">
-                    <input
-                      type="text"
-                      name="name"
-                      value={name}
-                      onChange={(e) => onChange(e)}
-                      id="inputName"
-                      className="form-control"
-                      placeholder="Name"
-                      required
-                    />
-                    <label for="inputName">Name</label>
+                        <div className="form-label-group">
+                          <input
+                            type="text"
+                            name="name"
+                            value={name}
+                            onChange={(e) => onChange(e)}
+                            id="inputName"
+                            className="form-control"
+                            placeholder="Name"
+                            required
+                          />
+                          <label for="inputName">Name</label>
+                        </div>
+                        <button
+                          className="btn btn-lg btn-primary btn-block text-uppercase"
+                          type="submit"
+                        >
+                          Sign Up!
+                        </button>
+                        <hr className="my-4" />
+                        <Link to="/login" style={{ textDecoration: "none" }}>
+                          <button
+                            className="btn btn-lg btn-google btn-block text-uppercase"
+                            type="submit"
+                          >
+                            <i className="fab mr-2"></i>Sign In Instead!
+                          </button>
+                        </Link>
+                      </form>
+                    </div>
                   </div>
-                  <button
-                    className="btn btn-lg btn-primary btn-block text-uppercase"
-                    type="submit"
-                  >
-                    Sign Up!
-                  </button>
-                  <hr className="my-4" />
-                  <Link to="/login" style={{ textDecoration: "none" }}>
-                    <button
-                      className="btn btn-lg btn-google btn-block text-uppercase"
-                      type="submit"
-                    >
-                      <i className="fab mr-2"></i>Sign In Instead!
-                    </button>
-                  </Link>
-                </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+
+    // <div>
+    //   <Navbar />
+    //   <div className="register">
+    //     <div className="row mr-0 ml-0">
+    //       <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+    //         <div data-aos="fade-right" className="card card-signin my-5">
+    //           <div className="card-body">
+    //             <h5 className="card-title text-center">Sign Up</h5>
+    //             <form className="form-signin" onSubmit={onSubmitForm}>
+    //               <div className="form-label-group">
+    //                 <input
+    //                   type="email"
+    //                   name="email"
+    //                   value={email}
+    //                   onChange={(e) => onChange(e)}
+    //                   id="inputEmail"
+    //                   className="form-control"
+    //                   placeholder="Email address"
+    //                   required
+    //                   autofocus
+    //                 />
+    //                 <label for="inputEmail">Email address</label>
+    //               </div>
+
+    //               <div className="form-label-group">
+    //                 <input
+    //                   type="password"
+    //                   name="password"
+    //                   value={password}
+    //                   onChange={(e) => onChange(e)}
+    //                   id="inputPassword"
+    //                   className="form-control"
+    //                   placeholder="Password"
+    //                   required
+    //                 />
+    //                 <label for="inputPassword">Password</label>
+    //               </div>
+
+    //               <div className="form-label-group">
+    //                 <input
+    //                   type="password"
+    //                   name="confirmPassword"
+    //                   value={confirmPassword}
+    //                   onChange={(e) => onChange(e)}
+    //                   id="inputConfPassword"
+    //                   className="form-control"
+    //                   placeholder="Confirm Password"
+    //                   required
+    //                 />
+    //                 <label for="inputConfPassword">Confirm Password</label>
+    //               </div>
+
+    //               <div className="form-label-group">
+    //                 <input
+    //                   type="text"
+    //                   name="name"
+    //                   value={name}
+    //                   onChange={(e) => onChange(e)}
+    //                   id="inputName"
+    //                   className="form-control"
+    //                   placeholder="Name"
+    //                   required
+    //                 />
+    //                 <label for="inputName">Name</label>
+    //               </div>
+    //               <button
+    //                 className="btn btn-lg btn-primary btn-block text-uppercase"
+    //                 type="submit"
+    //               >
+    //                 Sign Up!
+    //               </button>
+    //               <hr className="my-4" />
+    //               <Link to="/login" style={{ textDecoration: "none" }}>
+    //                 <button
+    //                   className="btn btn-lg btn-google btn-block text-uppercase"
+    //                   type="submit"
+    //                 >
+    //                   <i className="fab mr-2"></i>Sign In Instead!
+    //                 </button>
+    //               </Link>
+    //             </form>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
