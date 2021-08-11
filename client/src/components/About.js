c6e23e8783450cb43abb23e24e12cd87afcd9f17
casey <casey.khaled@gmail.com>
2021-08-11 21:16:43 +0700

    Rework on home page, and rework on about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,154 +1,127 @@
 import Navbar from "./partials/Navbar";
+import "../css/home.css"
 
 const About = () => {
   return (
     <div>
       <Navbar />
       <section data-aos="fade-right" data-aos-anchor-placement="top-bottom">
-        <div class="container">
+        <div className="container">
           <h1
             className="p-5 text-center font-weight-bold"
-            style={{ fontFamily: "Courier New, monospace", fontSize: "45px" }}
-          >
-            What is <span style={{ color: "#21fc21" }}>AnyWallet</span>?
-          </h1>
-          <p className="text-center">
-            <strong style={{ color: "#21fc21" }}>AnyWallet</strong> is the
-            world's{" "}
-            <strong style={{ fontSize: "1.5em", color: "#fcdc21" }}>#1</strong>{" "}
-            expense tracker tool that helps keep track of your everyday
-            expenses!
-          </p>
-          <h1
-            className="p-5 text-center font-weight-bold"
-            style={{ fontFamily: "Courier New, monospace", fontSize: "45px" }}
+            style={{ fontSize: "45px" }}
           >
             OUR TEAM
           </h1>
-          <div class="row align-items-center">
-            <div class="col-lg-6 order-lg-2">
-              <div class="p-5">
+          <div className="row align-items-center">
+            <div className="col-lg-6 order-lg-2">
+              <div className="p-5">
                 <img
-                  class="img-fluid rounded-circle"
+                  className="img-fluid rounded-circle"
                   src="assets/img/01.png"
                   alt="..."
                 />
               </div>
             </div>
-            <div class="col-lg-6 order-lg-1">
-              <div class="p-5">
-                <h2 class="display-4">Eric</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-                  aliquid, mollitia odio veniam sit iste esse assumenda amet
-                  aperiam exercitationem, ea animi blanditiis recusandae!
-                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
-                </p>
+            <div className="col-lg-6 order-lg-1">
+              <div className="p-5">
+                <h1 className="display-2">Eric</h1>
+                <h2>
+                  "There is beauty in simplicity"
+                </h2>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <section data-aos="fade-right" data-aos-anchor-placement="center-bottom">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-6">
-              <div class="p-5">
+      <section data-aos="fade-left" data-aos-anchor-placement="center-bottom">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6">
+              <div className="p-5">
                 <img
-                  class="img-fluid rounded-circle"
+                  className="img-fluid rounded-circle"
                   src="assets/img/02.png"
                   alt="..."
                 />
               </div>
             </div>
-            <div class="col-lg-6">
-              <div class="p-5">
-                <h2 class="display-4">Arief</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-                  aliquid, mollitia odio veniam sit iste esse assumenda amet
-                  aperiam exercitationem, ea animi blanditiis recusandae!
-                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
-                </p>
+            <div className="col-lg-6">
+              <div className="p-5">
+                <h1 className="display-2">Casey</h1>
+                <h2>
+                  "AnyWallet is a part of me"
+                </h2>
               </div>
             </div>
           </div>
         </div>
       </section>
       <section data-aos="fade-right" data-aos-anchor-placement="center-bottom">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-6 order-lg-2">
-              <div class="p-5">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6 order-lg-2">
+              <div className="p-5">
                 <img
-                  class="img-fluid rounded-circle"
+                  className="img-fluid rounded-circle"
                   src="assets/img/03.png"
                   alt="..."
                 />
               </div>
             </div>
-            <div class="col-lg-6 order-lg-1">
-              <div class="p-5">
-                <h2 class="display-4">Asep</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-                  aliquid, mollitia odio veniam sit iste esse assumenda amet
-                  aperiam exercitationem, ea animi blanditiis recusandae!
-                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
-                </p>
+            <div className="col-lg-6 order-lg-1">
+              <div className="p-5">
+                <h1 className="display-2">Arief</h1>
+                <h2>
+                  "We prepare millenials for their retirement."
+                </h2>
               </div>
             </div>
           </div>
         </div>
       </section>
 
-      <section data-aos="fade-right" data-aos-anchor-placement="center-bottom">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-6">
-              <div class="p-5">
+      <section data-aos="fade-left" data-aos-anchor-placement="center-bottom">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6">
+              <div className="p-5">
                 <img
-                  class="img-fluid rounded-circle"
-                  src="assets/img/04.jpg"
+                  className="img-fluid rounded-circle"
+                  src="assets/img/04.png"
                   alt="..."
                 />
               </div>
             </div>
-            <div class="col-lg-6">
-              <div class="p-5">
-                <h2 class="display-4">Ilham</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-                  aliquid, mollitia odio veniam sit iste esse assumenda amet
-                  aperiam exercitationem, ea animi blanditiis recusandae!
-                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
-                </p>
+            <div className="col-lg-6">
+              <div className="p-5">
+                <h1 className="display-2">Ilham</h1>
+                <h2>
+                  "Say goodbye to impulsive buying for all of us."
+                </h2>
               </div>
             </div>
           </div>
         </div>
       </section>
       <section data-aos="fade-right" data-aos-anchor-placement="center-bottom">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-6 order-lg-2">
-              <div class="p-5">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6 order-lg-2">
+              <div className="p-5">
                 <img
-                  class="img-fluid rounded-circle"
-                  src="assets/img/05.jpg"
+                  className="img-fluid rounded-circle"
+                  src="assets/img/05.png"
                   alt="..."
                 />
               </div>
             </div>
-            <div class="col-lg-6 order-lg-1">
-              <div class="p-5">
-                <h2 class="display-4">Casey</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-                  aliquid, mollitia odio veniam sit iste esse assumenda amet
-                  aperiam exercitationem, ea animi blanditiis recusandae!
-                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
-                </p>
+            <div className="col-lg-6 order-lg-1">
+              <div className="p-5">
+                <h1 className="display-2">Asep</h1>
+                <h2>
+                  "No more scraping your pocket for change at the end of the month"
+                </h2>
               </div>
             </div>
           </div>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,27 +6,75 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <header
-        style={{ height: "100vh" }}
+      <div
         data-aos="fade-right"
-        class="masthead text-center text-white"
+        className="masthead text-center text-white"
       >
-        <div class="masthead-content">
-          <div class="container">
-            <h1 class="masthead-heading mb-0">AnyWallet</h1>
-            <h2 class="masthead-subheading mb-0">Expense Tracker</h2>
-            <Link to="/about" style={{ textDecoration: "none" }}>
-              <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#!">
-                Learn More
+        <div className="masthead-content">
+          <div className="container">
+            <h1 className="masthead-heading mb-0">AnyWallet</h1>
+            <h2 className="masthead-subheading mt-1">Satu solusi. Tuk sadari keuanganmu.</h2>
+            <Link to="/register" style={{ textDecoration: "none" }}>
+              <a className="btn btn-primary btn-xl rounded-pill mt-5" href="#!">
+                Try Now!
               </a>
             </Link>
           </div>
         </div>
-        <div class="bg-circle-1 bg-circle"></div>
-        <div class="bg-circle-2 bg-circle"></div>
-        <div class="bg-circle-3 bg-circle"></div>
-        <div class="bg-circle-4 bg-circle"></div>
-      </header>
+        <div className="bg-circle-1 bg-circle"></div>
+        <div className="bg-circle-2 bg-circle"></div>
+        <div className="bg-circle-3 bg-circle"></div>
+        <div className="bg-circle-4 bg-circle"></div>
+        <section id="why-us" className="masthead-content">
+          <div className="container-fluid" data-aos="fade-up">
+            <div className="row">
+              <div className="col-lg-12 d-flex flex-column justify-content-center align-items-stretch mt-5">
+                <div className="content">
+                  <h3><strong>Kenapa Harus Sadar Keuangan?</strong></h3>
+                  <p>
+                    "Perasaan kemarin masih ada uang di dompet..." <br />
+                    "Recehan yang kusimpan banyak, kok udah kosong aja ya?" <br />
+                    "Yah, gagal lagi deh target nabung bulan ini..." <br /><br />
+                    Ke mana saja uangmu terpakai, berapa yang kamu keluarkan, dan pembelian yang terjadi secara spontan.<br />
+                    Dengan menyadari semuanya, kamu bisa mengendalikan pengeluaranmu.<br />
+                    Tidak kecewa lagi barang impianmu tidak terbeli. Tidak ada lagi kebutuhan mendesak yang tidak sanggup kamu bayar. <br></br>
+                    Kami bantu kamu.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <footer>
+        <div className="footer-top">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6 col-md-6 footer-contact text-white">
+                <h3>AnyWallet</h3>
+                <p>
+                  Jl. P. Mangkubumi <br />
+                  Gowongan, Kec. Jetis <br />
+                  Kota Yogyakarta, DIY 55233 <br />
+                  Indonesia <br /><br />
+                  <strong>Phone:</strong> +62 852 3213 1123 <br />
+                  <strong>Email:</strong> anywallet@gmail.com <br />
+                </p>
+              </div>
+              <div className="col-lg-6 col-md-6 footer-links text-white">
+                <h4>Our Social Networks</h4>
+                <div className="social-links mt-3">
+                  <a href="#" className="twitter"><i class="fab fa-twitter"></i></a>
+                  <a href="#" className="facebook"><i class="fab fa-facebook"></i></a>
+                  <a href="#" className="instagram"><i class="fab fa-instagram"></i></a>
+                  <a href="#" className="skype"><i class="fab fa-skype"></i></a>
+                  <a href="#" className="linkedin"><i class="fab fa-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      </div>
     </div>
   );
 };
